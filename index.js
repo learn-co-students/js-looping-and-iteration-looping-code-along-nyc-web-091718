94411bbb2a06c18ad421517c072ccaa3c5d08996
Shawna Davis <shawna_davis@alum.mit.edu>
2018-10-29 13:23:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(`Welcome ${array[i]}! You are employee #${i+1}.`);
+    }
+
+    return array;
+}
+
+function tailsNeverFails(){
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+
+  let counter = 0
+  while (maybeTrue()) {
+    ++counter;
+  }
+  return `You got ${counter} tails in a row!`
+}
